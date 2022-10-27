51ddf6891677787314c0232b3664e31b3053fc55
Polina <Polina_piter88@mail.ru>
2022-10-28 00:25:36 +0300

    2 задача

---
+++ Task2/Program.cs
@@ -0,0 +1,59 @@
+﻿/*Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+ и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+17 -> такого числа в массиве нет*/
+//получение числа с консоли
+int GetNumber(string message)
+{
+    Console.WriteLine(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+int[,] InitMatrix(int m, int n)
+{
+    int[,] resultMatrix = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            resultMatrix[i, j] = rnd.Next(-10, 20);
+        }
+    }
+    return resultMatrix;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int m = GetNumber("Введите число m:");
+int n = GetNumber("Введите число n:");
+int[,] matrix = InitMatrix(m, n);
+PrintMatrix(matrix);
+Console.WriteLine("Введите координаты");
+int i = int.Parse(Console.ReadLine());
+int j = int.Parse(Console.ReadLine());
+if (i > m && j > n)
+    Console.Write("такого числа нет");
+else
+{
+    Console.Write($"искомый элемент - {matrix[i, j]} ");
+}
+
+
+
+
+
+
+
+
